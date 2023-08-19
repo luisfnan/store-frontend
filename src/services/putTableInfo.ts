9e6dd83f56fa854010b360a26e482369cc478684
luisfnan <fernando-9813@hotmail.com>
2023-08-19 13:55:15 -0400

    fixing bugs

--- src/services/putTableInfo.ts
+++ src/services/putTableInfo.ts
@@ -2,11 +2,47 @@ import axios from "axios";
 
 
 export async function putTableInfo(ednPoint: String, objects: any) {
-    const jsonData = JSON.stringify(objects)
+
+    const categories = {
+        name: objects.name,
+        description: objects.description
+    }
+
+    const supplier = {
+        company_name: objects.company_name,
+        phoneNumber: objects.phoneNumber,
+        address: objects.address,
+        contact_name: objects.contact_name
+    }
+
+    const products = {
+        name: objects.name,
+        price: objects.price,
+        stock: objects.stock,
+    }
+
+    let result: string
+
+    if (ednPoint === "categories") {
+        result = JSON.stringify(categories)
+    } else if (ednPoint === "products") {
+        result = JSON.stringify(products)
+        console.log(result)
+    } else if (ednPoint === "suppliers") {
+        result = JSON.stringify(supplier)
+    } else {
+        result = JSON.stringify(objects)
+    }
+
+
+
     const id = objects.id;
     const apiUrl = `https://store-api-l9ki.onrender.com/api/${ednPoint}/${id}`
 
-    axios.put(apiUrl, jsonData, {
+
+
+
+    axios.put(apiUrl, result, {
         headers: {
             'Content-Type': 'application/json'
         }
